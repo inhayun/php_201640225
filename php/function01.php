d9368061281938512d9b9d708dacca80d452ceee
inhayun <dlsgk4564@naver.com>
2019-09-18 14:22:34 +0900

    sourcefree test

---
+++ php/function01.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "대림이";
+
+// 선언
+if(function_exists("hello")) {
+    function hello()
+    {
+        echo "반가와요 ";
+    }
+} 
+
+// 선언
+if(function_exists("hello")) {
+    function hello()
+    {
+        echo "반가와요 ";
+    }
+} 
+
+//호출
+hello();
+
+
